d81a9cdc3f9b396be642be6d8cdf9175658e0992
dmitry.b <dmitry.b@zala.com.au>
2024-02-02 14:18:59 +0200

    TDO1 enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 const PORT = 3002;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule, { cors: true });
   app
     .listen(PORT)
     .then(() => {
