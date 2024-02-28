015eb25c4451b661c65858f06877ffd3349db027
dmitry.b <dmitry.b@zala.com.au>
2024-02-28 14:09:08 +0200

    change port

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const PORT = 3002;
+const PORT = 3333;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
